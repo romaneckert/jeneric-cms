a90e5a1179498359afa9dab886da5f6fabba0171
Roman Eckert <mail@romaneckert.de>
2018-10-29 21:58:40 +0100

    work on sign in

--- src/handler/jeneric/sign-in.js
+++ src/handler/jeneric/sign-in.js
@@ -1,7 +1,28 @@
+let bcrypt = require('bcrypt');
+
 class Login {
 
-    handle(req, res) {
+    handle(req, res, next) {
 
+        this.model.user.findOne({ email: req.body.email }, (err, user) => {
+            if (err) return next(err);
+
+            if (null === user) {
+                return res.render('jeneric/sign-in', {
+                    errors: ['can not login, wrong credentials']
+                });
+            }
+
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                if (err) return next(err);
+
+                core.service.accessToken.addCookie(user, res);
+
+                return res.redirect('/user/overview/');
+
+            });
+
+        });
 
         return res.render('jeneric/sign-in');
     }
