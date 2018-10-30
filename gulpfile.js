4ae94069bb122c16788c60098d2906b860de73ea
Roman Eckert <mail@romaneckert.de>
2018-10-30 19:55:00 +0100

    add gulp file

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./view/scss/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
