3d2f84d0238fa75c2fb11000bf0c5e8ce53af6fb
Roman Eckert <mail@romaneckert.de>
2019-02-07 21:45:19 +0100

    optimize step-1 install structure

--- src/handler/jeneric/install/step-1.js
+++ src/handler/jeneric/install/step-1.js
@@ -10,50 +10,48 @@ class Step1 {
 
         if (form.submitted) {
 
-            if (form.valid) {
-
-                // check if user with email already exists
-                try {
-                    if (null !== await this.model.user.findOne({ email: form.instance.email })) {
-                        return res.redirect('/jeneric/install/step-2');
-                    }
-                } catch (err) {
-
-                    if (err) this.logger.error('can not find user with email', err);
-
-                    return res.render('jeneric/install/step-1', {
-                        form: form
-                    });
-                }
-
-                try {
-                    user.password = await bcrypt.hash(user.password, 10);
-                    user = await form.instance.save();
-                } catch (err) {
-
-                    if (err) this.logger.error('can not generate password for user', err);
-
-                    return res.render('jeneric/install/step-1', {
-                        form: form
-                    });
-                }
-
-                this.module.mail.send({
-                    to: user.email,
-                    subject: res.trans('test'),
-                    html: res.render('jeneric/user/email/verify')
-                }, (err) => {
-                    if (err) this.logger.error('can not send email to user', err);
-                });
-
-                return res.redirect('/jeneric/install/step-2');
-
-            } else {
+            if (!form.valid) {
 
                 return res.render('jeneric/install/step-1', {
                     form: form
                 });
             }
+
+            // check if user with email already exists
+            try {
+                if (null !== await this.model.user.findOne({ email: form.instance.email })) {
+                    return res.redirect('/jeneric/install/step-2');
+                }
+            } catch (err) {
+
+                if (err) this.logger.error('can not find user with email', err);
+
+                return res.render('jeneric/install/step-1', {
+                    form: form
+                });
+            }
+
+            try {
+                user.password = await bcrypt.hash(user.password, 10);
+                user = await form.instance.save();
+            } catch (err) {
+
+                if (err) this.logger.error('can not generate password for user', err);
+
+                return res.render('jeneric/install/step-1', {
+                    form: form
+                });
+            }
+
+            this.module.mail.send({
+                to: user.email,
+                subject: res.trans('test'),
+                html: res.render('jeneric/user/email/verify')
+            }, (err) => {
+                if (err) this.logger.error('can not send email to user', err);
+            });
+
+            return res.redirect('/jeneric/install/step-2');
 
         } else {
 
